8ffcaabea9e501012db255fdfcbf48a7160577c6
danieldUKIM <danield@feit.ukim.edu.mk>
2017-09-22 19:31:33 +0200

    delete docker network before build

--- rebuild_docker.sh
+++ rebuild_docker.sh
@@ -1,4 +1,5 @@
 sudo docker-compose down
 sudo docker rm -f $(sudo docker ps -a -q)
 sudo docker rmi -f $(sudo docker images -q)
+sudo docker network rm $(sudo docker network ls -q)
 sudo docker-compose up
